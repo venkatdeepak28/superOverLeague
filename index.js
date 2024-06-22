14dd69f33ecf95bc27956b3819de3363e6bd85da
venkatdeepak28 <tvenkatadeepak@gmail.com>
2024-06-22 06:19:51 +0000

    first react project

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='bg-container'>
+    <h1 className='main-heading'>Super Over League</h1>
+    <div className='img-container'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        className='img'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        className='img'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
